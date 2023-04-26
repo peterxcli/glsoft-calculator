d71bb0c24ef92ea9a2a86e724f4607926dc72f77
peterxcli <peterxcli@gmail.com>
2023-04-27 02:13:40 +0800

    change base to glsoft-calculator

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,9 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <base href='/glsoft-calculator'></base>
+      </Head>
       <body>
         <Main />
         <NextScript />
