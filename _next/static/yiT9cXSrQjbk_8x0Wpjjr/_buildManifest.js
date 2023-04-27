0ac4d78a85963359d200ae9f5a2e83962e55b5ee
peterxcli <peterxcli@users.noreply.github.com>
2023-04-27 10:22:58 +0000

    deploy: 7a9660ae3c6e62d406dd0e3fceddf4f724960bc2

--- _next/static/yiT9cXSrQjbk_8x0Wpjjr/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/78d6f6522c8d09a5.css","static/chunks/pages/index-1af6da2e7de65745.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
